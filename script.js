0f7a9e8348663dbb629c86cea914617349b155dd
NIDHEESH SN <nidheeshedappon0479@gmail.com>
2024-07-17 19:26:38 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("ageForm");
 const h1 = document.getElementById("showAge");
-const restBtn = document.getElementById("resetbtn")
+const restBtn = document.getElementById("reset-btn")
 const userName = document.getElementById("username");
 const email = document.getElementById("email");
 const regemail =
@@ -10,21 +10,25 @@ form.addEventListener("submit", async function validate(event) {
   event.preventDefault();
   let valid = true;
   if (!regExName.test(userName.value)) {
-    alert("must have a username");
+    alert("must have a name");
     userName.focus();
     valid = false;
+    return;
   }
   if (!regemail.test(email.value)) {
     alert("must have a email");
     email.focus();
     valid = false;
+    return;
   }
   if (!valid) {
     return;
   }
   try {
     let res = await fetch(`https://api.agify.io?name=${userName.value}`);
+    console.log(res);
     let displayAge = await res.json();
+    console.log(displayAge)
     h1.textContent = `Your Age is ${displayAge.age}`;
     if(!res.ok){
       h1.textContent= "something went wrong please try again"
@@ -39,4 +43,11 @@ restBtn.addEventListener("click", function() {
   email.value = ""
 
 })
+
+// fetch(`https://api.agify.io?name=${userName.value}`).then((result)=>{
+//     console.log(result)
+// })
+// .catch((error)=>{
+//     console.log("error",error)
+// })
 
